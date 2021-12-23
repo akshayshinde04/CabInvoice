c11fb3c90ccfc6680af8fc021d068f6d14e5dbd7
unknown <akshayHVshinde04@gmail.com>
2021-12-23 23:26:08 +0530

    UC2_Multiple

--- src/Test/TestInvoice.java
+++ src/Test/TestInvoice.java
@@ -2,6 +2,7 @@ package Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import com.bl.InvoiceGenrator;
+import com.bl.Rides;
 
 public class TestInvoice {
 	@Test
@@ -19,6 +20,15 @@ public class TestInvoice {
 		int time = 10;
 		double fare = invoiceGen.CalculateFare(distance,time);
 		assertEquals(25, fare, 0.0);
-	}	
+	}
+	@Test
+	public void distanceAndTimeShouldReturnMultipleRides() {
+		InvoiceGenrator invoiceGen = new InvoiceGenrator();
+		Rides [] ride = {new Rides(2.0,5),
+						new Rides(0.1,1) };
+		double fare = invoiceGen.calculateFare(ride);
+		assertEquals(30,fare,0.0);
+		}
 }
+	
 
---
+++ src/com/bl/Rides.java
@@ -0,0 +1,11 @@
+package com.bl;
+
+public class Rides {
+	public final double distances;
+	public final int time;
+	
+	public Rides(double distance, int time) {
+		this.distances = distance ;
+		this.time = time ;
+	}
+}
